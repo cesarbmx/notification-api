fc8dc2da7b207b507cc994065922b7cfb89e9053
Cesar C <cesarbmx@hotmail.com>
2024-02-01 19:26:06 +0100

    CORS

--- CesarBmx.Notification.Api/Startup.cs
+++ CesarBmx.Notification.Api/Startup.cs
@@ -52,9 +52,6 @@ namespace CesarBmx.Notification.Api
             // Mvc
             services.ConfigureMvc(Configuration);
 
-            // Cors
-            services.ConfigureCors(Configuration);
-
             // Health
             services.ConfigureHealth(Configuration);
 
