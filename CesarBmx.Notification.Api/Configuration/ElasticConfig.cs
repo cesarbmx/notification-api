2ddd497d35063a141c7a0d6954dffd32bb34331e
cesarc <cesarc@pinnacle.com>
2025-03-04 21:24:48 +0100

    ConfigureElastic

---
+++ CesarBmx.Notification.Api/Configuration/ElasticConfig.cs
@@ -0,0 +1,15 @@
+﻿using CesarBmx.Shared.Api.Configuration;
+
+
+namespace CesarBmx.Notification.Api.Configuration
+{
+    public static class ElasticConfig
+    {
+        public static IServiceCollection ConfigureElastic(this IServiceCollection services)
+        {
+            services.ConfigureSharedElastic();
+
+            return services;
+        }
+    }
+}
